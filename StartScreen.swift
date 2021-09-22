223a39833e40d865cd9abfb1987256e3025f6c07
Theo Davis <57919329+tdcoder17@users.noreply.github.com>
2021-09-22 09:56:03 -0500

    Add files via upload

---
+++ StartScreen.swift
@@ -0,0 +1,20 @@
+//
+//  StartScreen.swift
+//  Nerd Football Quiz
+//
+//  Created by Theo Davis on 3/1/21.
+//
+
+import UIKit
+
+class StartScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
